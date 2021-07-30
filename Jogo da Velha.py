8fe9c695dbebbf984d8e3c3e0417eb11dd3eae3e
adelaide-adurens <adelaideadurens@gmail.com>
2021-07-30 08:59:44 -0300

    Correçao 3

--- Jogo da Velha.py
+++ Jogo da Velha.py
@@ -4,8 +4,7 @@ import re
 def exibeTabuleiro(tabuleiro):
     print(*tabuleiro[0][0],*tabuleiro[0][1], *tabuleiro[0][2])
     print(*tabuleiro[1][0],*tabuleiro[1][1], *tabuleiro[1][2])
-    print(*tabuleiro[2][0],*tabuleiro[2][1], *tabuleiro[2][2])
-    print()
+    print(*tabuleiro[2][0],*tabuleiro[2][1], *tabuleiro[2][2], "\n")
 
 # Função entradaValida recebe o input do usuário e verifica se está entre as opções válidas, retornando True ou False.
 def entradaValida(entrada):
